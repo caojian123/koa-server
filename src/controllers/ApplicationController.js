e58fa00e635a909b18a44ab66101d25b336d15c8
jeffreycao <jeffreycao@vipabc.com>
2019-04-16 18:10:37 +0800

    fix

--- src/controllers/ApplicationController.js
+++ src/controllers/ApplicationController.js
@@ -3,35 +3,28 @@ import jsonwebtoken from 'jsonwebtoken';
 import applicationService from '../service/ApplicationService';
 
 const applicationSchema = joi.object({
-    appSn: joi
+    app_sn: joi
         .string()
         .max(36),
-    appName: joi
+    app_name: joi
         .string()
         .min(1)
         .max(32),
-    deptCode: joi
+    dept_code: joi
         .string()
         .min(3)
         .max(16),
 });
 
-const queryParamsMap = {
-    appSn: 'app_sn',
-    appName: 'app_name',
-    deptCode: 'dept_code'
-};
+
 class ApplicationController {
     async applicationController(ctx) {
         const request = ctx.request.body;
         const validator = joi.validate(request, applicationSchema);
-        let params = {};
         
         if (validator.error) ctx.renderError(400, validator.error.details[0].message);
-        Object.keys(request).map(key => {
-            params[queryParamsMap[key]] = request[key];
-        });
-        const data = await applicationService.queryApplicationList(params);
+       
+        const data = await applicationService.queryApplicationList(request);
 
         return ctx.send(data); 
     }
